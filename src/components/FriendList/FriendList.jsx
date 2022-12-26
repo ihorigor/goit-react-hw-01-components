e862ba25db94350cf4d79ae984747804e92b11ef
ihorigor <103446738+ihorigor@users.noreply.github.com>
2022-12-26 19:10:50 +0100

    TEST

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,29 +1,29 @@
-import friends from './friends.json';
-import PropTypes from 'prop-types';
+// import friends from './friends.json';
+// import PropTypes from 'prop-types';
 
-export const FriendList = ({ friends }) => {
-  return (
-    <ul className="friend-list">
-      {friends.map(({ avatar, name, isOnline, id }) => {
-        return (
-          <li className="item" key={id}>
-            <span className="status">{isOnline}</span>
-            <img className="avatar" src={avatar} alt="User avatar" width="48" />
-            <p className="name">{name}</p>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+// export const FriendList = ({ friends }) => {
+//   return (
+//     <ul className="friend-list">
+//       {friends.map(({ avatar, name, isOnline, id }) => {
+//         return (
+//           <li className="item" key={id}>
+//             <span className="status">{isOnline}</span>
+//             <img className="avatar" src={avatar} alt="User avatar" width="48" />
+//             <p className="name">{name}</p>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
 
-FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.number.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      name: PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-    })
-  ),
-};
+// FriendList.propTypes = {
+//   friends: PropTypes.arrayOf(
+//     PropTypes.exact({
+//       id: PropTypes.number.isRequired,
+//       isOnline: PropTypes.bool.isRequired,
+//       name: PropTypes.string.isRequired,
+//       avatar: PropTypes.string.isRequired,
+//     })
+//   ),
+// };
